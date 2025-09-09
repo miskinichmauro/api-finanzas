2f5d356e2f76e8bcb5e9a4ab4ed9ccf4de480392
Mauro Miskinich <miskinichmauro@gmail.com>
2025-09-08 23:19:24 -0300

    Update Program.cs

--- src/ApiFinanzas.WebApi/Program.cs
+++ src/ApiFinanzas.WebApi/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
     {
         policy.AllowAnyOrigin()
             .AllowAnyHeader()
-            .AllowAnyMethod();
+            .AllowAnyMethod()
             .WithExposedHeaders("Content-Disposition");
 });
 });
